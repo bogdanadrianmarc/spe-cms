5b1245d88e3135304ec5b19dfae4c36ba10cce8f
tannousmarc <tannous.marc@gmail.com>
2018-01-29 17:42:05 +0000

    selections log final result on page leave, now have a choice # in front

--- SPE_cms/src/main/resources/com/spe/cms/front-end/js/selections.js
+++ SPE_cms/src/main/resources/com/spe/cms/front-end/js/selections.js
@@ -6,45 +6,36 @@ const selections = {
   <div id = "selections-page">
   <h1>Project selection order</h1>
   <h2>Drag and drop items in the order that you like.</h2>
-    <ul id="items">
-      <li>'The rock outside MVB' inc.</li>
-      <li>Green Foundation</li>
-      <li>Oracle Waiter</li>
-      <li>IBM Server Monitoring</li>
-      <li>University of Bristol RFID Laptops</li>
-      <li>Sailing Society Dashboard</li>
-    </ul>
+  <ol id="items">
+  <li>'The rock outside MVB' inc.</li>
+  <li>Green Foundation</li>
+  <li>Oracle Waiter</li>
+  <li>IBM Server Monitoring</li>
+  <li>University of Bristol RFID Laptops</li>
+  <li>Sailing Society Dashboard</li>
+  </ol>
   </div>`,
   data: function () {
-  return {
-    selections: [
-
-    ]
-  }
-},
+    return {
+      selections: []
+    }
+  },
   mounted: function(){
     var el = document.getElementById('items');
+    let self = this;
     var sortable = Sortable.create(el,{
-      store: {
-    		/**
-    		 * Get the order of elements. Called once during initialization.
-    		 * @param   {Sortable}  sortable
-    		 * @returns {Array}
-    		 */
-    		get: function (sortable) {
-    			var order = localStorage.getItem(sortable.options.group.name);
-    			return order ? order.split('|') : [];
-    		},
-
-    		/**
-    		 * Save the order of elements. Called onEnd (when the item is dropped).
-    		 * @param {Sortable}  sortable
-    		 */
-    		set: function (sortable) {
-    			var order = sortable.toArray();
-    			console.log(order);
-    		}
-    	}
+      animation: 0,
+      sort: true,
+      onEnd: function (/**Event*/evt) {
+        var itemEl = evt.item;  // dragged HTMLElement
+        self.selections = [...evt.to.children].map(element => {
+          return element.innerHTML;
+        });
+      },
     });
+  },
+  beforeRouteLeave (to, from, next) {
+    console.log(this.selections);
+    next();
   }
 };
