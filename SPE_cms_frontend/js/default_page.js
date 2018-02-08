f70728b28f2963ad28c619e91e1a7332b217a99c
Sibela Chinareva <sc16913@it000417.users.bris.ac.uk>
2018-02-08 14:44:25 +0000

    tried doing login

--- SPE_cms_frontend/js/default_page.js
+++ SPE_cms_frontend/js/default_page.js
@@ -1,6 +1,53 @@
 const default_page = {
+  data: function () {
+    return {
+      username: null,
+      password: null
+    }
+  },
+  created: function(){
+    let self = this;
+  },
+  methods: {
+    clickBTN: function(username, password){
+      let self = this;
+      console.log(username, password)
+      $.ajax({
+        url: 'http://localhost:8080/projects',
+        method: 'POST',
+        data: {username, password},
+        success: function (data) {
+          console.log(data);
+        },
+        error: function (error) {
+          console.log(error);
+        }
+      });
+    },
+    isUndefined: function(item){
+      return typeof item === "undefined";
+    }
+  },
   template: `
   <div id = "user-dashboard">
-    Placeholder default page
+
+    <div id = "welcome-image">
+      <h1>SPE 2018</h1>
+      <img src = "img/university-on-film.jpg"/>
+    </div>
+
+    <div id = "login-info">
+      <label class="control-label" for="username">Username</label>
+      <input v-model="username" type="text" id = "username" placeholder="edit me">
+      <label class="control-label" for="password">Password</label>
+      <input v-model="password" type="text" id = "password" placeholder="edit me">
+      <p>Username is: {{ username }}</p>
+      <p>Password is: {{ password }}</p>
+    </div>
+
+    <div class = "select-project-btn">
+      <button v-on:click="clickBTN(username, password)"><span>Log in</span> </button>
+    </div>
+
   </div>`
 };
