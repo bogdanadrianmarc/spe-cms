95f0846651ab6b9e6794702c9511cf49666da9cc
Marc Tannous <tannous.marc@gmail.com>
2018-02-11 02:36:03 +0000

    send POST for swapping elements, needs backend

--- SPE_cms_frontend/js/selections.js
+++ SPE_cms_frontend/js/selections.js
@@ -4,22 +4,22 @@
 const selections = {
   template: `
   <div id = "selections-page">
-    <h1>Project selection</h1>
-    <p>
-    Sort projects you would like to work on in your preferred order.<br />
-    The first three projects will have the highest priority, but all other submitted choices will be taken into account as well.
-    </p>
-    <ol id="items">
-      <li v-for = "selection in selections" v-bind:key = "selection.priority">
-        {{ selection.title }}
-        <button  v-on:click = "removeSelection(selection.id)"><i class="fa fa-times" aria-hidden="true"></i></button>
-      </li>
-    </ol>
-    <router-link to = "/projects">
-      <div class = "add">
-        <i class="fa fa-plus" aria-hidden="true"></i>
-      </div>
-    </router-link>
+  <h1>Project selection</h1>
+  <p>
+  Sort projects you would like to work on in your preferred order.<br />
+  The first three projects will have the highest priority, but all other submitted choices will be taken into account as well.
+  </p>
+  <ol id="items">
+  <li v-for = "selection in selections" v-bind:key = "selection.priority">
+  {{ selection.title }}
+  <button  v-on:click = "removeSelection(selection.id)"><i class="fa fa-times" aria-hidden="true"></i></button>
+  </li>
+  </ol>
+  <router-link to = "/projects">
+  <div class = "add">
+  <i class="fa fa-plus" aria-hidden="true"></i>
+  </div>
+  </router-link>
   </div>`,
   data: function () {
     return {
@@ -36,7 +36,6 @@ const selections = {
         login_token: "whvwbvwxghqw!whvwbvwxghqw"
       },
       success: function (dataSelections) {
-        console.log("DataS:" + dataSelections);
         $.ajax({
           url: 'http://localhost:8080/projects',
           method: 'POST',
@@ -44,7 +43,16 @@ const selections = {
             login_token: "whvwbvwxghqw!whvwbvwxghqw"
           },
           success: function (dataProjects) {
-            self.selections = dataSelections.map(select => dataProjects[select.projectId]);
+            // sort based on priority
+            dataSelections = dataSelections.sort(function(a, b){
+              return a.priority - b.priority;
+            });
+            // map project ids to actual project objects
+            self.selections = dataSelections.map(select => {
+              let returnObj = dataProjects[select.projectId];
+              returnObj.priority = dataSelections.filter(obj => obj.projectId === select.projectId)[0].priority;
+              return returnObj;
+            });
           },
           error: function (error) {
             console.log(error);
@@ -63,17 +71,101 @@ const selections = {
     var sortable = Sortable.create(el,{
       animation: 0,
       sort: true,
-      onEnd: function (/**Event*/evt) {
-        var itemEl = evt.item;  // dragged HTMLElement
-        self.selections = [...evt.to.children].map(element => {
-          return element.innerHTML;
+      onEnd: function (evt) {
+        //TODO: TO BE IMPLEMENTED ON BACKEND!
+        $.ajax({
+          url: 'http://localhost:8080/selection_swap',
+          method: 'POST',
+          data: {
+            studentId: "test_student",
+            login_token: "whvwbvwxghqw!whvwbvwxghqw",
+            oldPos: evt.oldIndex,
+            newPos: evt.newIndex
+          },
+          success: function (data) {
+            console.log(data);
+          },
+          error: function (error) {
+            console.log(error);
+          }
         });
+        // swap on display
+        // const old = evt.oldIndex;
+        // const nou = evt.newIndex;
+        // let oldBackup = self.selections[old];
+        // let newBackup = self.selections[nou];
+        // self.selections[nou] = oldBackup;
+        // self.selections[old] = newBackup;
+        // swap on backend
+        // $.ajax({
+        //   url: 'http://localhost:8080/selection_delete',
+        //   method: 'POST',
+        //   data: {
+        //     studentId: "test_student",
+        //     // -1 accounts for difference in zero-indexing on backend and one-indexing on frontend
+        //     projectId: oldBackup.id,
+        //     login_token: "whvwbvwxghqw!whvwbvwxghqw"
+        //   },
+        //   success: function (data) {
+        //     console.log(data);
+        //   },
+        //   error: function (error) {
+        //     console.log(error);
+        //   }
+        // });
+        // $.ajax({
+        //   url: 'http://localhost:8080/selection_delete',
+        //   method: 'POST',
+        //   data: {
+        //     studentId: "test_student",
+        //     // -1 accounts for difference in zero-indexing on backend and one-indexing on frontend
+        //     projectId: newBackup.id,
+        //     login_token: "whvwbvwxghqw!whvwbvwxghqw"
+        //   },
+        //   success: function (data) {
+        //     console.log(data);
+        //   },
+        //   error: function (error) {
+        //     console.log(error);
+        //   }
+        // });
+        //
+        // $.ajax({
+        //   url: 'http://localhost:8080/selection_save',
+        //   method: 'POST',
+        //   data: {
+        //     studentId: "test_student",
+        //     // -1 accounts for difference in zero-indexing on backend and one-indexing on frontend
+        //     projectId: oldBackup.id,
+        //     priority: newBackup.priority,
+        //     login_token: "whvwbvwxghqw!whvwbvwxghqw"
+        //   },
+        //   success: function (data) {
+        //     console.log(data);
+        //   },
+        //   error: function (error) {
+        //     console.log(error);
+        //   }
+        // });
+        // $.ajax({
+        //   url: 'http://localhost:8080/selection_save',
+        //   method: 'POST',
+        //   data: {
+        //     studentId: "test_student",
+        //     // -1 accounts for difference in zero-indexing on backend and one-indexing on frontend
+        //     projectId: newBackup.id,
+        //     priority: oldBackup.priority,
+        //     login_token: "whvwbvwxghqw!whvwbvwxghqw"
+        //   },
+        //   success: function (data) {
+        //     console.log(data);
+        //   },
+        //   error: function (error) {
+        //     console.log(error);
+        //   }
+        // });
       },
     });
-  },
-  beforeRouteLeave (to, from, next) {
-    console.log(this.selections);
-    next();
   },
   methods: {
     removeSelection: function(id){
