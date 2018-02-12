6a11ad1cb2590cf4c3a9ecef93bdf6e580ca62b9
sibela46 <sibelachinareva@gmail.com>
2018-02-12 18:11:13 +0000

    changed header and tried doing remove project

---
+++ SPE_cms_frontend/js-teacher/projects_list.js
@@ -0,0 +1,143 @@
+const projects_list = {
+  data:function(){
+    return {
+      projectList: [],
+      projectListCopy: [],
+      login_token: "",
+      // 1 for descending, -1 for ascending
+      sortOrder: 1,
+      currPriority: 1
+    }
+  },
+  created: function(){
+    let self = this;
+    self.$parent.loading = true;
+    $.ajax({
+     url: 'http://localhost:8080/login',
+     method: 'POST',
+     data: {
+       type: "student",
+       username: "test_student",
+       password: "test_student"
+     },
+     success: function (data) {
+       self.login_token = data;
+       // console.log(data);
+       $.ajax({
+         url: 'http://localhost:8080/projects',
+         method: 'POST',
+         data: {
+           login_token: data
+         },
+         success: function (data) {
+           self.projectList = data;
+           self.projectListCopy = data;
+           self.$parent.loading = false;
+         },
+         error: function (error) {
+           console.log(error);
+           self.$parent.loading = false;
+         }
+       });
+
+       $.ajax({
+         url: 'http://localhost:8080/selections_id',
+         method: 'POST',
+         data: {
+           id: "test_student",
+           login_token: "whvwbvwxghqw!whvwbvwxghqw"
+         },
+         success: function (dataSelections) {
+           self.currPriority = dataSelections.length +1;
+         },
+         error: function (error) {
+           console.log(error);
+         }
+       });
+ }
+});
+  },
+  methods: {
+    sortApplicants: function() {
+      if(this.sortOrder === 1)
+      this.projectList.sort(this.applicantsAscending);
+      else
+      this.projectList.sort(this.applicantsDescending);
+      this.sortOrder = this.sortOrder * (-1);
+    },
+    applicantsAscending(a, b){
+      return a.applicantsNr > b.applicantsNr ? 1 : -1;
+    },
+    applicantsDescending(a, b){
+      return a.applicantsNr < b.applicantsNr ? 1 : -1;
+    },
+    sortTitle: function(){
+      const field = document.querySelector("input[name=title-input]").value.replace(/ /g,'').toUpperCase();
+      this.projectList = this.projectListCopy.filter(function(project){
+        if(field === "")
+        return project;
+        let title = project.title.replace(/ /g,'');
+        title = title.toUpperCase();
+        if(title.startsWith(field))
+        return project;
+      });
+    },
+    incrementPriority: function(){
+      this.currPriority += 1;
+    },
+    decreasePriority: function(){
+      this.currPriority -= 1;
+    },
+    sortTags: function(){
+      const field = document.querySelector("input[name=tags-input]").value;
+
+      this.projectList = this.projectListCopy.filter(function(project){
+        if(field === "")
+        return project;
+        // split tags by comma, remove whitespaces
+        let searchingFor = field.replace(/ /g,'').split(',').map((word) => {
+          return word.toUpperCase();
+        });
+        const searchingIn = project.tags.split(',').map((word) => {
+          return word.toUpperCase();
+        });
+
+        let notFound = false;
+        let notFoundAtAll = true;
+        for(let i = 0; i < searchingFor.length; i++){
+          notFoundAtAll = true;
+          for(let j = 0; j < searchingIn.length; j++){
+            if(searchingIn[j].startsWith(searchingFor[i]))
+            notFoundAtAll = false;
+          }
+          if(notFoundAtAll){
+            notFound = true;
+            break;
+          }
+        }
+        if(!notFound)
+        return project;
+      });
+    }
+  },
+  template: `
+  <div id = "projects-list">
+    <!-- loader -->
+    <transition name = "fade" mode = "out-in">
+      <div v-if = "this.$parent.loading" class = "loader" key="loading">
+        <loader_spinner></loader_spinner>
+      </div>
+      <div v-else key="loaded">
+        <projects_list_filters></projects_list_filters>
+        <transition-group name="sort-list">
+          <projects_list_item
+            v-for = "project in this.projectList"
+            v-bind:projects = "project"
+            v-bind:key = "project.id"
+            :priority = "currPriority">
+          </projects_list_item>
+        </transition-group>
+      </div>
+    </transition>
+  </div>`
+};
---
+++ SPE_cms_frontend/js-teacher/projects_list_item.js
@@ -0,0 +1,82 @@
+Vue.component('projects_list_item', {
+  props: ['projects','priority'],
+  data: function(){
+    return {
+      tagColors: [],
+    }
+  },
+  created: function(){
+    this.$parent.$parent.$parent.getTagColors(this.projects,this);
+    let self = this;
+    $.ajax({
+      url: 'http://localhost:8080/selections_id',
+      method: 'POST',
+      data: {
+        id: "test_student",
+        login_token: "whvwbvwxghqw!whvwbvwxghqw"
+      },
+      error: function (error) {
+        console.log(error);
+      }
+    });
+  },
+  methods: {
+    clickBTN: function(index){
+      let self = this;
+      //projects_list.projectList.splice(index, 1);
+        $.ajax({
+          url: 'http://localhost:8080/project_delete',
+          method: 'POST',
+          data: {
+            studentId: "test_student",
+            // -1 accounts for difference in zero-indexing on backend and one-indexing on frontend
+            projectId: index-1,
+            login_token: "whvwbvwxghqw!whvwbvwxghqw"
+          },
+          success: function (data) {
+            self.$parent.$parent.decreasePriority();
+            console.log(data);
+          },
+          error: function (error) {
+            console.log(error);
+          }
+        });
+      //this.currPriority += 1;
+    },
+    isUndefined: function(item){
+      return typeof item === "undefined";
+    }
+  },
+  template: `<div class = "project-list-item">
+    <img class = "project-list-item-image" alt = "Project Image" v-bind:src="projects.imgUrl" />
+    <div class = "project-list-item-content">
+    <div class = "tag-container" v-for = "(tag, index) in projects.tags.split(',')">
+    <div class = "tag" v-bind:class="tagColors[index]"> {{tag}} </div>
+    </div>
+    <h1>  {{ projects.title }}  </h1>
+    <!-- truncate string -->
+    <p> {{ projects.content.substring(0,500) + "..." }} </p>
+    <div class = "project-list-item-applicants">
+    No. of applicants <span class = "applicants"> {{ projects.applicantsNr }}</span>
+    </div>
+    <div class = "project-list-item-id">
+    Project #{{ projects.id }}
+    </div>
+    <div style="clear:both"></div>
+    <hr />
+    <!-- TODO: Discuss in meeting -->
+    <div class = "project-list-item-license">
+    <img src = "img/mit-license.png" />
+    <p>MIT License</p>
+    </div>
+
+    <div class = "select-project-btn">
+    <button v-on:click="clickBTN(projects.id)">Remove project</button>
+    </div>
+
+    <router-link v-bind:to="projects.projectUrl">
+    </i><div class = "learn-more-btn">Details</div>
+    </router-link>
+    </div>
+  </div>`
+});
