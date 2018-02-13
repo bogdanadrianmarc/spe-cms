c27f45f0168bc1ba12b6b770cfb8e89e73efecc5
Bogdan Adrian Marc <marc.bogdan96@yahoo.com>
2018-02-13 13:45:17 +0000

    code structure for future spring boot tests

---
+++ SPE_cms/src/test/java/com/spe/cms/restcontroller/PreferenceRESTControllerTests.java
@@ -0,0 +1,16 @@
+package com.spe.cms.restcontroller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PreferenceRESTControllerTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
