d7ffaca62bc5e0e08834bd21adb7a08990b0f808
tannousmarc <tannous.marc@gmail.com>
2018-02-15 12:24:30 +0000

    separate style for homepage

--- SPE_cms_frontend/js/default_page.js
+++ SPE_cms_frontend/js/default_page.js
@@ -19,13 +19,14 @@ const default_page = {
   },
   template: `
   <div class = "default-page">
-    <div class = "welcome-image">
+    <div class = "hero">
       <h2>Dear Student,</h2>
       <h1>Welcome to SPE 2018</h1>
       <p> This is the place where you can browse and select projects posted by clients,<br>
       sort your choices in order of preference and submit your decision,</br>
       message your clients as well as your group partners, <br>
       and get feedback from lecturers.</p>
+    </div>
        <div class = "browse-students">
          <h2>Browse projects and start deciding.</h2>
          <router-link to = "/projects">
