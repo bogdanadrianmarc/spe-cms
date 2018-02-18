3a121cc7df8907122d22f54ece5487de48364c5e
Marc Tannous <tannous.marc@gmail.com>
2018-02-18 18:29:37 +0000

    client form front-end complete

--- SPE_cms_frontend/js/client/form_project.js
+++ SPE_cms_frontend/js/client/form_project.js
@@ -14,8 +14,8 @@ const form_project = {
       if(animating) return false;
       animating = true;
 
-      current_fs = $(this).parent();
-      next_fs = $(this).parent().next();
+      current_fs = $(this).parent().parent();
+      next_fs = $(this).parent().parent().next();
 
       //activate next step on progressbar using the index of next_fs
       $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
@@ -25,12 +25,6 @@ const form_project = {
       //hide the current fieldset with style
       current_fs.animate({opacity: 0}, {
         step: function(now, mx) {
-          //as the opacity of current_fs reduces to 0 - stored in "now"
-          //1. scale current_fs down to 80%
-          scale = 1 - (1 - now) * 0.2;
-          //2. bring next_fs from the right(50%)
-          left = (now * 50)+"%";
-          //3. increase opacity of next_fs to 1 as it moves in
           opacity = 1 - now;
           current_fs.css({
             'transform': 'scale('+scale+')',
@@ -38,7 +32,7 @@ const form_project = {
           });
           next_fs.css({'left': left, 'opacity': opacity});
         },
-        duration: 800,
+        duration: 350,
         complete: function(){
           current_fs.hide();
           animating = false;
@@ -52,8 +46,8 @@ const form_project = {
       if(animating) return false;
       animating = true;
 
-      current_fs = $(this).parent();
-      previous_fs = $(this).parent().prev();
+      current_fs = $(this).parent().parent();
+      previous_fs = $(this).parent().parent().prev();
 
       //de-activate current step on progressbar
       $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
@@ -63,23 +57,17 @@ const form_project = {
       //hide the current fieldset with style
       current_fs.animate({opacity: 0}, {
         step: function(now, mx) {
-          //as the opacity of current_fs reduces to 0 - stored in "now"
-          //1. scale previous_fs from 80% to 100%
-          scale = 0.8 + (1 - now) * 0.2;
-          //2. take current_fs to the right(50%) - from 0%
-          left = ((1-now) * 50)+"%";
-          //3. increase opacity of previous_fs to 1 as it moves in
           opacity = 1 - now;
           current_fs.css({'left': left});
           previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
         },
-        duration: 800,
+        duration: 350,
         complete: function(){
           current_fs.hide();
           animating = false;
         },
         //this comes from the custom easing plugin
-        easing: 'easeInOutBack'
+        easing: 'easeInSine'
       });
     });
 
@@ -101,36 +89,42 @@ const form_project = {
       </ul>
       <fieldset>
         <h2 class="fs-title">Personal Details</h2>
-        <h3 class="fs-subtitle">This is step 1</h3>
+        <h3 class="fs-subtitle">Details about the organisation's contact person.</h3>
 
-        <input type="text" name="firstname" placeholder="First Name" />
-        <input type="text" name="lastname" placeholder="Last Name" />
-        <input type="text" name="phone" placeholder="Phone" />
-        <input type="text" name="email" placeholder="E-mail" />
-        <input type="button" name="next" class="next action-button" value="Next" />
+        <input type="text" class = "half" name="firstname" placeholder="First Name" />
+        <input type="text" class = "half" name="lastname" placeholder="Last Name" />
+        <input type="text" class = "half" name="phone" placeholder="Phone" />
+        <input type="text" class = "half" name="email" placeholder="E-mail" />
+        <div class = "line">
+          <input type="button" class = "half" name="next" class="next action-button" value="Next" />
+        </div>
       </fieldset>
 
       <fieldset>
         <h2 class="fs-title">Organisation Details</h2>
-        <h3 class="fs-subtitle">Details about company</h3>
-        <input type="text" name="twitter" placeholder="Organisation Name" />
-        <input type="text" name="facebook" placeholder="Industry" />
-        <input type="text" name="gplus" placeholder="Country" />
-        <input type="text" name="twitter" placeholder="City" />
-        <input type="text" name="facebook" placeholder="Zip Code" />
-        <input type="text" name="gplus" placeholder="Address" />
-        <input type="button" name="previous" class="previous action-button" value="Previous" />
-        <input type="button" name="next" class="next action-button" value="Next" />
+        <h3 class="fs-subtitle">Details about the organisation.</h3>
+        <input type="text" class = "half" name="orgname" placeholder="Organisation Name" />
+        <input type="text" class = "half" name="industry" placeholder="Industry" />
+        <input type="text" class = "third" name="country" placeholder="Country" />
+        <input type="text" class = "third" name="city" placeholder="City" />
+        <input type="text" class = "third" name="zipcode" placeholder="Zip Code" />
+        <textarea name="address" placeholder="Address"></textarea>
+        <div class = "line">
+          <input type="button" class = "half" name="previous" class="previous action-button" value="Previous" />
+          <input type="button" class = "half" name="next" class="next action-button" value="Next" />
+        </div>
       </fieldset>
 
       <fieldset>
-        <h2 class="fs-title">Brief title</h2>
-        <h3 class="fs-subtitle">Here comes the big one</h3>
-        <input type="text" name="fname" placeholder="Project Name" />
-        <input type="text" name="lname" placeholder="Tags separated by commas" />
-        <textarea name="address" placeholder="Detailed project description"></textarea>
-        <input type="button" name="previous" class="previous action-button" value="Previous" />
-        <input type="submit" name="submit" class="submit action-button" value="Submit" />
+        <h2 class="fs-title">Project Brief</h2>
+        <h3 class="fs-subtitle">Details regarding the project, that will be showcased to students.</h3>
+        <input type="text" class = "half" name="projname" placeholder="Project Name" />
+        <input type="text" class = "half" name="tags" placeholder="Tags separated by commas" />
+        <textarea name="projdesc" class = "big" placeholder="Detailed project description"></textarea>
+        <div class = "line">
+          <input type="button" class = "half" name="previous" class="previous action-button" value="Previous" />
+          <input type="submit" class = "half" name="submit" class="submit action-button" value="Submit" />
+        </div>
       </fieldset>
     </form>
   </div>`
