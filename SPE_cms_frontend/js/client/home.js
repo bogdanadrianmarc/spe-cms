263a5347e9772b5ff5df0c58d5ba2263349237c6
sibela46 <sibelachinareva@gmail.com>
2018-03-19 21:31:29 +0000

    changed description for clients, notifications not needed here yet

--- SPE_cms_frontend/js/client/home.js
+++ SPE_cms_frontend/js/client/home.js
@@ -17,9 +17,11 @@ const default_page = {
       <div class="tint"></div>
       <div class="text">
         <h1>Software Product Engineering</h1>
-        <p>Nostrud
-        coniunctione est proident, laborum fore incurreret sed aute fabulas a quem
-        noster, admodum elit singulis.</p>
+        <p>Welcome to the page where you can add projects
+        for this unit, edit them and also contact the
+        students that will be working for you in the future.
+        So what are you waiting for? Start adding projects by
+        pressing the button below.</p>
         <router-link to="projects">
           <button>Projects</button>
         </router-link>
