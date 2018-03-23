7f74793c71817f3303eae179c11c8a266ab475ff
sibela46 <sibelachinareva@gmail.com>
2018-03-23 10:20:49 +0000

    Second release

--- SPE_cms_frontend/js/start/home.js
+++ SPE_cms_frontend/js/start/home.js
@@ -1,17 +1,38 @@
 const default_page = {
   data: function () {
     return {
-      username: null,
-      password: null
+      type: ""
     }
   },
   created: function(){
+
   },
   methods: {
-    getLoginDetails: function() {
-      var username = document.getElementById("firstname").value;
-      var password = document.getElementById("password").value;
-      console.log(username, password);
+    check: function() {
+      var username = document.getElementById('username').value;
+      var password = document.getElementById('password').value;
+      if (username == "sc" && password == "hey"){
+        type = "student";
+      }
+      if (username == "ab" && password == "hey"){
+        type = "teacher";
+      }
+      if (username == "cd" && password == "hey"){
+        type = "client";
+      }
+
+      if (type == "student"){
+        window.open("studentView.html", "_self");
+      }
+      else if (type == "teacher"){
+        window.open("teacherView.html", "_self");
+      }
+      else if (type == "client"){
+        window.open("clientView.html", "_self");
+      }
+      else{
+        swal({text:"Incorrect details", dangerMode: true});
+      }
     }
   },
   template: `
@@ -21,10 +42,10 @@ const default_page = {
       <div class="text">
         <h1>Software Product Engineering</h1>
         <form id="login" onsubmit="return false;">
-        <input type="text" id="firstname" placeholder="Username" />
+        <input type="text" id="username" placeholder="Username" />
         <input type="password" id="password" placeholder="Password" />
         </form>
-        <button class="start" v-on:click="getLoginDetails()">Log in</button>
+        <button class = "start" v-on:click = "check()">Log in</button>
       </div>
     </div>
   </div>`
