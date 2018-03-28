41ff564dd5db781b70c21a4535d42e352fe6b715
sibela46 <sibelachinareva@gmail.com>
2018-03-28 16:31:58 +0100

    front page matches backend data except from id

--- SPE_cms_frontend/js/start/home.js
+++ SPE_cms_frontend/js/start/home.js
@@ -7,12 +7,26 @@ const default_page = {
       type: "",
       attributes: [],
       types: ["student", "teacher", "client"],
-      showDropDown: false
+      clientDetails: "",
+      teacherDetails: "",
+      showFieldsClient: false,
+      showFieldTeacher: false
     }
   },
-  created: function(){
-  },
   methods: {
+    onChange : function(data){
+        var selectedType = document.getElementById('type').value;
+        if (selectedType == this.types[2]){
+          this.showFieldsClient = true;
+        }
+        else if (selectedType == this.types[1]){
+          this.showFieldTeacher = true;
+        }
+        else {
+          this.showFieldsClient = false;
+          this.showFieldTeacher = false;
+        }
+    },
     login: function() {
       let self = this;
       self.username = document.getElementById('username').value;
@@ -49,20 +63,46 @@ const default_page = {
       }
       });
    },
-   checkEmpty(username, password, email){
+   checkDetailsEmpty(array){
+     for (var i=0; i<array.length; i++){
+       console.log(this.type);
+       if (array[i] == "" && this.type == "client"){
+         console.log("here");
+         return true;
+       }
+     }
+     if (this.type == "teacher" && this.teacherDetails == ""){
+       return true;
+     }
+     else {
+       return false;
+     }
+   },
+   checkEmpty(username, password, email, details){
      return username == "" || password == "" || email == "";
    },
    isTypeCorrect: function(element){
      return this.type === element;
    },
+   getDetails: function() {
+     this.type = document.getElementById('type').value;
+     if (this.type == "client"){
+       this.clientDetails = document.getElementById('orgName').value + ";" + document.getElementById('orgPhone').value + ";" +
+       document.getElementById('orgAddress').value + ";" + document.getElementById('persName').value + ";" +
+       document.getElementById('persEmail').value + ";" + document.getElementById('persPhone').value;
+     }
+     else if (this.type == "teacher"){
+       this.teacherDetails = document.getElementById('email').value;
+     }
+   },
    register: function (){
      let self = this;
-     this.type = document.getElementById('type').value;
      var user = document.getElementById('user').value;
      var pass = document.getElementById('pass').value;
      var confirm_pass = document.getElementById('pass-confirm').value;
-     var email = document.getElementById('email').value;
-     if (this.checkEmpty(user, pass, email)){
+     this.getDetails();
+     if (this.checkEmpty(user, pass) || this.checkDetailsEmpty(this.clientDetails.split(";"))){
+       console.log("enter");
        swal("Please fill in all fields.", {dangerMode: true});
      }
      else if (pass != confirm_pass ){
@@ -74,9 +114,11 @@ const default_page = {
        swal("Choose an user from the dropdown menu.", {dangerMode: true});
      }
      else {
-       this.attributes = user + ";"
+
+       this.attributes = "0;" + user + ";"
                        + pass + ";"
-                       + email ;
+                       + this.clientDetails + ";"
+                       + this.teacherDetails;
        $.ajax({
          url: 'http://localhost:8080/register',
          method: 'POST',
@@ -109,23 +151,35 @@ const default_page = {
       <div class = "registration">
         <h2>Just getting started? Register here.</h2>
         <form id="register" onsubmit="return false;">
-          <h3>*Pick a fancy username</h3>
+          <h3>Pick a fancy username</h3>
             <input type="text" id="user" placeholder="Username" />
-          <h3>*Come up with a strong password</h3>
+          <h3>Come up with a strong password</h3>
             <input type="password" id="pass" placeholder="Password" />
             <input type="password" id="pass-confirm" placeholder="Confirm Password" />
-          <h3>*Enter your email address</h3>
+          <div v-if="this.showFieldTeacher">
+          <h3>Enter your email address</h3>
             <input type="email" id="email" placeholder="Email" />
-          <h3>*I'm a:</h3>
+          </div>
+          <h3>I'm a:</h3>
             <div class = "dropdown">
-            <select id="type">
+            <select id="type" @change="onChange(this)">
               <option disable hidden value = "">Type of user</option>
-              <option value = "student">Student</option>
-              <option value = "teacher">Teacher</option>
-              <option value = "client">Client</option>
+              <option v-for="type in types"> {{ type }}</option>
             </select>
             </div>
-          <h4>* All fields marked with this symbol are required.</h4>
+            <div id = "clientFields" v-if="this.showFieldsClient">
+              <div class = "leftField">
+              <input type="text" id="orgName" placeholder="Organisation Name" />
+              <input type="text" id="orgAddress" placeholder="Organisation Address" />
+              <input type="tel" id="orgPhone" placeholder="Organisation Number" />
+              </div>
+              <div class = "rightField">
+              <input type="text" id="persName" placeholder="Personal Name" />
+              <input type="tel" id="persPhone" placeholder="Personal Number" />
+              <input type="text" id="persEmail" placeholder="Personal Email" />
+              </div>
+            </div>
+          <h4>* All fields are required.</h4>
           <button v-on:click = "register()">Register</button>
         </form>
         </div>
